7405093c6aac24e4371f4d8ebeaaafd69b2369cd
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-05 19:23:55 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -32,7 +32,7 @@ server.put('/api/dogs/:id', (req, res) => {
 })
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 server.delete('/api/dogs/:id', (req, res) => {
-  
+  res.status(200).json({ message: `deleting dog with id ${req.params.id}` })
 })
 // EXPOSING THE SERVER TO OTHER MODULES
 module.exports = server
